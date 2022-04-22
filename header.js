890568921b82e72e48701504117bdbbfacdf07c6
Lucas Souza Dias <lucas.dias@cwi.com.br>
2022-04-22 00:01:07 -0300

    navigation header

---
+++ header.js
@@ -0,0 +1,5 @@
+const header = document.getElementById('header');
+header.innerHTML = `
+<a href="../ClosestPairOfPoints/ClosestPairOfPoints.html">Closest pair of points</a>
+<a href="../ConvexHull/ConvexHull.html">Convex hull</a>
+`;
