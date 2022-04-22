ede706dda6d527cbd8680546d1d9c473bd919b37
Lucas Souza Dias <lucas.dias@cwi.com.br>
2022-04-22 01:10:48 -0300

    adapted bruteForce for use on D&D

--- ClosestPairOfPoints/bruteForce.js
+++ ClosestPairOfPoints/bruteForce.js
@@ -70,3 +70,117 @@ function bruteForceMelhorado(points, startIndex, endIndex) {
     return [closest[0], closest[1], minDist, time, comparisons, calculated];
 }
 
+
+// ao invés de comparar todos com todos, comparo uma metade com a outra metade (separados pela mediana)
+function bruteForceAdaptadoDD(points, startIndex, median, endIndex) {
+    let length = endIndex - startIndex + 1;
+
+    if (length < 2) return [null, null, Infinity, 0, 1, 0];
+    if (length == 2) return [points[startIndex], points[startIndex + 1], dist(points[startIndex], points[startIndex + 1]), 0, 2, 1];
+
+    let minDist = Infinity;
+    let closest = [points[startIndex], points[startIndex + 1]];
+    let time = new Date().getTime();
+    let comparisons = 2;
+    let calculated = 0;
+
+    for (let i = startIndex; i <= median; i++) {
+        comparisons++;
+        for (let j = median + 1; j <= endIndex; j++) {
+            comparisons++;
+            let p = points[i], q = points[j];
+            let d = dist(p, q); calculated++;
+            if (d < minDist) {
+                comparisons++;
+                minDist = d;
+                closest[0] = p;
+                closest[1] = q;
+            }
+        } comparisons++;
+    } comparisons++;
+
+    time = new Date().getTime() - time;
+    return [closest[0], closest[1], minDist, time, comparisons, calculated];
+}
+
+// [0] = p0
+// [1] = p1
+// [2] = d
+function bruteForce_withoutCounters(points) {
+    let length = points.length;
+
+    if (length < 2) return [null, null, Infinity];
+    if (length == 2) return [points[0], points[1], dist(points[0], points[1])];
+
+    let minDist = Infinity;
+    let closest = [points[0], points[1]];
+
+    for (let i = 0; i < length; i++) {
+        for (let j = 0; j < length; j++) {
+            if (i == j) continue;
+            let p = points[i], q = points[j];
+            let d = dist(p, q);
+            if (d < minDist) {
+                minDist = d;
+                closest[0] = p;
+                closest[1] = q;
+            }
+        }
+    }
+
+    return [closest[0], closest[1], minDist];
+}
+
+function bruteForceMelhorado_withoutCounters(points, startIndex, endIndex) {
+    if (!startIndex) { startIndex = 0; }
+    if (!endIndex) { endIndex = points.length - 1; }
+    let length = endIndex - startIndex + 1;
+
+    if (length < 2) return [null, null, Infinity, 0, 1, 0];
+    if (length == 2) return [points[startIndex], points[startIndex + 1], dist(points[startIndex], points[startIndex + 1])];
+
+    let minDist = Infinity;
+    let closest = [points[startIndex], points[startIndex + 1]];
+
+    for (let i = startIndex; i <= endIndex; i++) {
+        for (let j = i + 1; j <= endIndex; j++) {
+            let p = points[i], q = points[j];
+            let d = dist(p, q);
+            if (d < minDist) {
+                minDist = d;
+                closest[0] = p;
+                closest[1] = q;
+            }
+        }
+    }
+
+    return [closest[0], closest[1], minDist];
+}
+
+
+// ao invés de comparar todos com todos, comparo uma metade com a outra metade (separados pela mediana)
+function bruteForceAdaptadoDD_withoutCounters(points, startIndex, median, endIndex) {
+    if (!startIndex) { startIndex = 0; }
+    if (!endIndex) { endIndex = points.length - 1; }
+    let length = endIndex - startIndex + 1;
+
+    if (length < 2) return [null, null, Infinity];
+    if (length == 2) return [points[startIndex], points[startIndex + 1], dist(points[startIndex], points[startIndex + 1])];
+
+    let minDist = Infinity;
+    let closest = [points[startIndex], points[startIndex + 1]];
+
+    for (let i = startIndex; i <= median; i++) {
+        for (let j = median + 1; j <= endIndex; j++) {
+            let p = points[i], q = points[j];
+            let d = dist(p, q);
+            if (d < minDist) {
+                minDist = d;
+                closest[0] = p;
+                closest[1] = q;
+            }
+        }
+    }
+
+    return [closest[0], closest[1], minDist];
+}
--- ClosestPairOfPoints/divideAndConquer.js
+++ ClosestPairOfPoints/divideAndConquer.js
@@ -4,7 +4,7 @@
 // [3] = execution time
 // [4] = relational operations
 // [5] = distance calculations
-function closestDivideAndConquer(unsortedPoints, startIndex, endIndex, melhoria) {
+function closestPairOfPointsDivideAndConquer(unsortedPoints, startIndex, endIndex, bruteForceAdaptado) {
     if (!startIndex) { startIndex = 0; }
     if (!endIndex) { endIndex = unsortedPoints.length - 1; }
 
@@ -13,14 +13,13 @@ function closestDivideAndConquer(unsortedPoints, startIndex, endIndex, melhoria)
     let minWidth = sortedPoints[startIndex].x;
     let maxWidth = sortedPoints[endIndex].x;
 
-    let result = divideAndConquer(sortedPoints, startIndex, endIndex, minWidth, maxWidth, melhoria);
+    let result = divideAndConquer(sortedPoints, startIndex, endIndex, minWidth, maxWidth, bruteForceAdaptado);
 
     time = new Date().getTime() - time;
     return [result[0], result[1], result[2], time, result[4], result[5]];
 }
 
-
-function divideAndConquer(P, i, n, s0, s1, melhoria) {
+function divideAndConquer(P, i, n, s0, s1, bruteForceAdaptado) {
     let partitionSize = n - i + 1;
     if (partitionSize < 2) return [null, null, Infinity, 0, 1, 0];
     if (partitionSize == 2) return [P[i], P[n], dist(P[i], P[n]), 0, 2, 1];
@@ -29,25 +28,58 @@ function divideAndConquer(P, i, n, s0, s1, melhoria) {
 
     let half = (s0 + s1) / 2;
     let m = 0;
-    for (m = i; P[m].x < half; m++) { comparisons++; } comparisons++;
 
-    let closestP1 = divideAndConquer(P, i, m, s0, half, melhoria);
-    let closestP2 = divideAndConquer(P, m + 1, n, half, s1, melhoria);
+    for (m = i; m + 1 <= n && P[m + 1].x < half; m++) { comparisons += 2; } comparisons++;
+
+    let closestP1 = divideAndConquer(P, i, m, s0, half, bruteForceAdaptado);
+    let closestP2 = divideAndConquer(P, m + 1, n, half, s1, bruteForceAdaptado);
 
     let closest = closestP1[2] < closestP2[2] ? closestP1 : closestP2; comparisons++;
 
-    let P3i, P3n, closestP3;
-    for (P3i = m; P3i >= i && half - P[P3i].x < closest[2]; P3i--) { comparisons += 2; } P3i++; comparisons += 2;
-    for (P3n = m + 1; P3n <= n && P[P3n].x - half < closest[2]; P3n++) { comparisons += 2; } P3n--; comparisons += 2;
+    let P2Remainder, P3Remainder, closestP3;
 
-    if (melhoria)
-        closestP3 = divideAndConquer(P, P3i, P3n, P[P3i].x, P[P3n].x, melhoria); // Recursão infinita, ainda não pensei em como fazer funcionar
+    for (P2Remainder = 0; m - 1 - P2Remainder >= i && half - P[m - 1 - P2Remainder].x < closest[2]; P2Remainder++) { comparisons += 2; } comparisons++;
+    for (P3Remainder = 0; m + 1 + P3Remainder <= n && P[m + 1 + P3Remainder].x - half < closest[2]; P3Remainder++) { comparisons += 2; } comparisons++;
+
+    if (bruteForceAdaptado)
+        closestP3 = bruteForceAdaptadoDD(P, m - P2Remainder, m, m + P3Remainder);
     else
-        closestP3 = bruteForceMelhorado(P, P3i, P3n); // TODO: fazer bruteForce adaptado
+        closestP3 = bruteForceMelhorado(P, m - P2Remainder, m + P3Remainder);
 
     closest = closestP3[2] < closest[2] ? closestP3 : closest; comparisons++;
 
     closest[4] = closestP1[4] + closestP2[4] + comparisons + closestP3[4];
     closest[5] = closestP1[5] + closestP2[5] + closestP3[5];
+    return closest;
+}
+
+
+function divideAndConquer_withoutCounters(P, i, n, s0, s1, bruteForceAdaptado) {
+    let partitionSize = n - i + 1;
+    if (partitionSize < 2) return [null, null, Infinity];
+    if (partitionSize == 2) return [P[i], P[n], dist(P[i], P[n])];
+
+    let half = (s0 + s1) / 2;
+    let m = 0;
+
+    for (m = i; m + 1 <= n && P[m + 1].x < half; m++) { }
+
+    let closestP1 = divideAndConquer(P, i, m, s0, half, bruteForceAdaptado);
+    let closestP2 = divideAndConquer(P, m + 1, n, half, s1, bruteForceAdaptado);
+
+    let closest = closestP1[2] < closestP2[2] ? closestP1 : closestP2;
+
+    let P2Remainder, P3Remainder, closestP3;
+
+    for (P2Remainder = 0; m - 1 - P2Remainder >= i && half - P[m - 1 - P2Remainder].x < closest[2]; P2Remainder++) { }
+    for (P3Remainder = 0; m + 1 + P3Remainder <= n && P[m + 1 + P3Remainder].x - half < closest[2]; P3Remainder++) { }
+
+    if (bruteForceAdaptado)
+        closestP3 = bruteForceAdaptadoDD(P, m - P2Remainder, m, m + P3Remainder);
+    else
+        closestP3 = bruteForceMelhorado(P, m - P2Remainder, m + P3Remainder);
+
+    closest = closestP3[2] < closest[2] ? closestP3 : closest;
+
     return closest;
 }
